c5d1e1325504144177ec1385bd00abff5cc1f93f
BPx33 <bradperbs@gmail.com>
2024-05-17 23:52:37 +0200

    test

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -49,7 +49,7 @@ export default function Header(){
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>  
         </header>
     )
 }
