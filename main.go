25bd7ef194c19932d1f21e99d79ca821828efc0c
BPx33 <bradperbs@gmail.com>
2024-05-18 19:19:58 +0200

    fix porta

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
     http.HandleFunc("/", handler)
-    log.Println("Starting server on :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Starting server on :3001")
+    log.Fatal(http.ListenAndServe(":3001", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
