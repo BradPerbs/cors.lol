d07d8391668ba7861fd505884ca2f4426923380a
BPx33 <bradperbs@gmail.com>
2024-05-18 15:17:46 +0200

    Update main.go

--- main.go
+++ main.go
@@ -44,8 +44,9 @@ func main() {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
+ 
 		
+
 		// Handle OPTIONS requests for CORS preflight
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
