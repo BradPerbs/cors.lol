affcf5caacc1cb8af3ee1e70f6d90fc7a1225a38
BPx33 <bradperbs@gmail.com>
2024-05-18 19:08:01 +0200

    fix

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "github.com/elazarl/goproxy"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/elazarl/goproxy"
 )
 
 const (
-    tcpPort = 3001
+	tcpPort = 3001
 )
 
-
 func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
 
-    proxy := goproxy.NewProxyHttpServer()
-    proxy.Verbose = true
+	// Handle HTTPS connect method
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 
-    // Handles HTTPS connections as well, Cloudflare will send these as HTTP to your server
-    proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-
-    addr := fmt.Sprintf(":%d", tcpPort)
-    err := http.ListenAndServe(addr, proxy)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
-        return
-    }
+	addr := fmt.Sprintf(":%d", tcpPort)
+	fmt.Println("Starting Proxy on", addr)
+	err := http.ListenAndServe(addr, proxy)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		return
+	}
 }
