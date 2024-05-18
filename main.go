8d6747a6b90b8d9ec2a5695f948b13e33355faf0
BPx33 <bradperbs@gmail.com>
2024-05-18 18:25:15 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/reiver/go-cors"
-
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 )
 
@@ -25,23 +26,34 @@ func main() {
 	fmt.Printf("http://proxy.cors:%d/http://example.com/user/joeblow.atom\n", tcpport)
 	fmt.Print("\n\n\n\n")
 
-	var handler http.Handler
-	{
-		proxy := cors.ProxyHandler{
-			LogWriter:os.Stdout,
-		}
-
-		handler = &proxy
-	}
-
-
-	{
-		var addr string = fmt.Sprintf(":%d", tcpport)
-
-		err := http.ListenAndServe(addr, handler)
-		if nil != err {
-			fmt.Fprintln(os.Stderr, "ERROR:", err)
+	proxyHandler := func(w http.ResponseWriter, r *http.Request) {
+		urlString := r.URL.Path[1:]
+		parsedURL, err := url.Parse(urlString)
+		if err != nil {
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
 			return
 		}
+
+		proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+		r.URL.Path = parsedURL.Path
+		r.URL.RawQuery = parsedURL.RawQuery
+
+		// Modify the request to enable CORS
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		r.Host = parsedURL.Host
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		proxy.ServeHTTP(w, r)
+	}
+
+	http.HandleFunc("/", proxyHandler)
+
+	addr := fmt.Sprintf(":%d", tcpport)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR:", err)
 	}
 }
