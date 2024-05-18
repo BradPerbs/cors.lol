b359f0a836e5810550e93f4a26d2c05ccfd4be3a
BPx33 <bradperbs@gmail.com>
2024-05-18 19:15:30 +0200

    fix

--- main.go
+++ main.go
@@ -1,29 +1,88 @@
-package main
+package pancors
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
-
-	"github.com/elazarl/goproxy"
+	"net/http/httputil"
+	"net/url"
 )
 
-const (
-	tcpPort = 3001
-)
+type corsTransport struct {
+	referer     string
+	origin      string
+	credentials string
+}
 
-func main() {
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
+func (t corsTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	// Put in the Referer if specified
+	if t.referer != "" {
+		r.Header.Add("Referer", t.referer)
+	}
 
-	// Handle HTTPS connect method
-	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-
-	addr := fmt.Sprintf(":%d", tcpPort)
-	fmt.Println("Starting Proxy on", addr)
-	err := http.ListenAndServe(addr, proxy)
+	// Do the actual request
+	res, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		return nil, err
+	}
+
+	res.Header.Set("Access-Control-Allow-Origin", t.origin)
+	res.Header.Set("Access-Control-Allow-Credentials", t.credentials)
+
+	return res, nil
+}
+
+func handleProxy(w http.ResponseWriter, r *http.Request, origin string, credentials string) {
+	// Check for the User-Agent header
+	if r.Header.Get("User-Agent") == "" {
+		http.Error(w, "Missing User-Agent header", http.StatusBadRequest)
 		return
+	}
+
+	// Get the optional Referer header
+	referer := r.URL.Query().Get("referer")
+	if referer == "" {
+		referer = r.Header.Get("Referer")
+	}
+
+	// Get the URL
+	urlParam := r.URL.Query().Get("url")
+	// Validate the URL
+	urlParsed, err := url.Parse(urlParam)
+	if err != nil {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	// Check if HTTP(S)
+	if urlParsed.Scheme != "http" && urlParsed.Scheme != "https" {
+		http.Error(w, "The URL scheme is neither HTTP nor HTTPS", http.StatusBadRequest)
+		return
+	}
+
+	// Setup for the proxy
+	proxy := httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL = urlParsed
+			r.Host = urlParsed.Host
+		},
+		Transport: corsTransport{referer, origin, credentials},
+	}
+
+	// Execute the request
+	proxy.ServeHTTP(w, r)
+}
+
+// HandleProxy is a handler which passes requests to the host and returns their
+// responses with CORS headers
+func HandleProxy(w http.ResponseWriter, r *http.Request) {
+	handleProxy(w, r, "*", "true")
+}
+
+// HandleProxyFromHosts is a handler which passes requests only from specified to the host
+func HandleProxyWith(origin string, credentials string) func(http.ResponseWriter, *http.Request) {
+	if !(credentials == "true" || credentials == "false") {
+		log.Panicln("Access-Control-Allow-Credentials can only be 'true' or 'false'")
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		handleProxy(w, r, origin, credentials)
 	}
 }
