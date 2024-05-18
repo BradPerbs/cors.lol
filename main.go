0747e1358e023de8ac532de2ca6948fdb55e4db8
BPx33 <bradperbs@gmail.com>
2024-05-18 19:23:44 +0200

    ratelimit

--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
     "io"
     "log"
     "net/http"
+    "sync"
+    "time"
+)
+
+var (
+    // Limit each IP to 100 requests per 30 minutes
+    rateLimit = 100
+    rateLimitDuration = 30 * time.Minute
+    requestCounts = make(map[string]int)
+    countsLock = sync.Mutex{}
+    // Max allowed size of the request body is 10MB
+    maxBodySize int64 = 10 << 20 // 10 MB
 )
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", limitRate(limitSize(handler)))
+
     log.Println("Starting server on :3001")
     log.Fatal(http.ListenAndServe(":3001", nil))
 }
@@ -51,3 +64,35 @@ func handler(w http.ResponseWriter, r *http.Request) {
     io.Copy(w, resp.Body)
 }
 
+func limitRate(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        ip := r.RemoteAddr
+        countsLock.Lock()
+        count, exists := requestCounts[ip]
+        if !exists || count >= rateLimit {
+            requestCounts[ip] = 1
+            go func() {
+                time.Sleep(rateLimitDuration)
+                countsLock.Lock()
+                delete(requestCounts, ip)
+                countsLock.Unlock()
+            }()
+        } else {
+            requestCounts[ip]++
+        }
+        countsLock.Unlock()
+
+        if count >= rateLimit {
+            http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+            return
+        }
+        next(w, r)
+    }
+}
+
+func limitSize(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        r.Body = http.MaxBytesReader(w, r.Body, maxBodySize)
+        next(w, r)
+    }
+}
