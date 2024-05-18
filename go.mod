711031853815f8d93a9c0b928ca4aca5ad7a9283
BPx33 <bradperbs@gmail.com>
2024-05-18 18:33:38 +0200

    fix

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module myproxy
 go 1.22.3
 
 require (
+	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,68 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-	"strings"
+    "fmt"
+    "net/http"
+    "os"
+    "github.com/elazarl/goproxy"
 )
 
 const (
-	tcpport = 3001
+    tcpPort = 3001
 )
 
 func main() {
-	fmt.Println("CORS Proxy")
-	fmt.Println("")
-	fmt.Println("In your /etc/hosts file add this line:")
-	fmt.Println("")
-	fmt.Println("127.0.0.1\tproxy.cors")
-	fmt.Println("")
-	fmt.Println("And then run a request against:")
-	fmt.Println("")
-	fmt.Printf("http://proxy.cors:%d/http://example.com/user/joeblow.atom\n", tcpport)
-	fmt.Print("\n\n\n\n")
+    fmt.Println("CORS Proxy")
+    fmt.Println("")
+    fmt.Println("In your /etc/hosts file add this line:")
+    fmt.Println("")
+    fmt.Println("127.0.0.1\tproxy.cors")
+    fmt.Println("")
+    fmt.Println("And then run a request against:")
+    fmt.Println("")
+    fmt.Printf("http://proxy.cors:%d/http://example.com/user/joeblow.atom\n", tcpPort)
+    fmt.Print("\n\n\n\n")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Trim the leading '/' from the request URL
-		originalURL := r.URL.Path[1:]
+    proxy := goproxy.NewProxyHttpServer()
+    proxy.Verbose = true
 
-		if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
-			http.Error(w, "URL must start with http:// or https://", http.StatusBadRequest)
-			return
-		}
+    // Handles HTTPS connections as well, Cloudflare will send these as HTTP to your server
+    proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 
-		parsedURL, err := url.Parse(originalURL)
-		if err != nil {
-			http.Error(w, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-
-		proxy := httputil.NewSingleHostReverseProxy(parsedURL)
-
-		// Modify the request to enable CORS
-		r.URL = parsedURL
-		r.Host = parsedURL.Host
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		proxy.ServeHTTP(w, r)
-	})
-
-	addr := fmt.Sprintf(":%d", tcpport)
-	log.Printf("Starting server on %s", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR:", err)
-		os.Exit(1)
-	}
+    addr := fmt.Sprintf(":%d", tcpPort)
+    err := http.ListenAndServe(addr, proxy)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+        return
+    }
 }
